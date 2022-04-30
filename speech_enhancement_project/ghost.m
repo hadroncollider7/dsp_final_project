0a9b885be73c28017779435e6219bfed49a07d5f
Hadroncollider <chaulo74@students.rowan.edu>
2022-04-30 16:39:03 -0400

    Update work

--- speech_enhancement_project/ghost.m
+++ speech_enhancement_project/ghost.m
@@ -1,36 +1,24 @@
+%**********************************************
+% This is the main workspace for the project.
+% ********************************************%
 %% Ex. Listen to a speech file
 clear variables;clf;clc;close all;
 y=load_or_audioread('sp01.wav');
-soundsc(y)
-%%
+soundsc(y,8000)
+%% Listen to the noise
 y=load('street.dat');
 soundsc(y,8000)
-%% 
+%% Add noise to a clean speech signal
 clear variables;clf;clc;close all;
 [t]=addnoisex('sp01.wav','street.dat',30,'s01_noisy_snr30.wav');
-%% 
+%% Listen to speech data
 soundsc(t,8000)
 %% Part 6. Adding noise to a speech
 clear variables;clf;clc;close all;
-s_clean=load_or_audioread('sp01.wav'); % clean speech
-noise=load_or_audioread('white.dat');  % noise
 
-SNR=30;
-% L2 norm of clean speech
-pspeech=norm(s_clean,2);
-
-% L2 norm of desired noise
-pDesiredNoise=pspeech/(10^(SNR/20));
-
-% L2 norm of read-in noise signal
-pnoise=norm(noise,2);
-% adjust L2 norm of the noise to be pdesirednoise
-noise=pnoise.*(pDesiredNoise)/pnoise;
-%add t=s+noise
-t=s_clean+noise;
+SNR=10;
+[t]=addnoisex('sp01.wav','street.dat',SNR,'spXX_noisey_snrXX.wav');
 soundsc(t,8000);
-% [t]=addnoisex('sp01.wav','white.dat',SNR,'sp01_noisey_snr30.wav');
-% soundsc(t,8000);
 
 
 
